aff338da16749df10bf2f9e6a9177d0906649aa6
Michal Scienski <mscienski@overstock.com>
2016-02-05 14:23:08 -0700

    complete application

--- voting-client/src/action_creators.js
+++ voting-client/src/action_creators.js
@@ -7,7 +7,19 @@ export function setState(state) {
 
 export function vote(entry) {
     return {
+        meta: {
+            remote: true
+        },
         type: 'VOTE',
         entry
     };
+}
+
+export function next() {
+    return {
+        meta: {
+            remote: true
+        },
+        type: 'NEXT'
+    }
 }
